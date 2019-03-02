a3da479bc9244eb87ef569a6b07a781f0f928fd5
Daan de Ruiter <daanjderuiter@hotmail.nl>
2019-03-02 16:42:50 +0100

    Changed solarsystem to use custom Vector class

--- solarsystem.py
+++ solarsystem.py
@@ -1,6 +1,7 @@
-import numpy as np
+from math import sqrt
 import matplotlib.pyplot as plt
-from scipy.constants import G
+from scipy.constants import G, au
+from vector import Vector
 
 class Universe:
 
@@ -17,7 +18,7 @@ class Universe:
             print(f'There is no planet with the name {name}!')
 
     def add_body(self, planet):
-        name = planet.get_name()
+        name = planet.name
         if name in self.planets:
             raise NameError(f'A planet with the name {name} already exists!')
         else:
@@ -31,41 +32,73 @@ class Universe:
 
     def forces(self):
         n = len(self.planets)
-        forces = np.zeros((n, n, 3))
-        planets = list(self.planets.keys())
-
-        planet_1 = planet_2 = None
-
+        forces = {}
         # Only running j from i halves the amount of required computations
-        for i in range(n):
-            planet_1 = self.planets[planets[i]]
+        for planet in self.planets.values():
+            total_force = Vector(0, 0, 0)
 
-            for j in range(i, n):
-                planet_2 = self.planets[planets[j]]
-                vector = planet_1.position - planet_2.position
+            for other_planet in self.planets.values():
+                if planet is other_planet:
+                    continue
 
-                forces[i, j] = -G * planet_1.mass * planet_2.mass * \
-                        vector / np.linalg.norm(vector)**3 
-                forces[j, i] = forces[i, j]
+                vector = planet.position - other_planet.position
+                total_force += -G * planet.mass * other_planet.mass * \
+                        vector / vector.norm()**3 
 
-        return dict(zip(planets, forces.sum(axis=0)))
+            forces[planet.name] = total_force
+
+        return forces
 
     def update(self):
         for planet, force in self.forces().items():
-
             acceleration = force / self.planets[planet].mass
 
-            self.planets[planet].velocity, self.planets[planet].position = \
-                    acceleration * self.dt, self.planets[planet].velocity * self.dt
+            self.planets[planet].velocity += acceleration * self.dt
+            self.planets[planet].position += self.planets[planet].velocity * self.dt
 
 
 class Planet:
     def __init__(self, name, mass, position, velocity):
         self.name = name
         self.mass = mass
-        self.position = position
-        self.velocity = velocity
+        self.position = Vector(*position)
+        self.velocity = Vector(*velocity)
 
     def __repr__(self):
-        return f'Name: {self.name}\nMass: {self.mass} kg\nPosition: {tuple(self.position)}\nVelocity: {tuple(self.velocity)}'
+        return f'Name: {self.name}\nMass: {self.mass} kg\nPosition: ' + \
+                f'{tuple(self.position)}\nVelocity: {tuple(self.velocity)}'
+
+
+
+# Demo
+M_sun = 2e30
+M_earth = 5.972e24
+M_mars = 6.39e23
+
+universe = Universe()
+earth = Planet('Earth', M_earth, (au, 0, 0), (0, sqrt(G*M_sun / au), 0))
+sun = Planet('Sun', M_sun, (0, 0, 0), (0, 0, 0))
+mars = Planet('Mars', M_mars, (1.524*au, 0, 0), (0, 24131, 0))
+
+universe.add_body(earth)
+universe.add_body(sun)
+universe.add_body(mars)
+
+r_sun = []
+r_earth = []
+r_mars = []
+
+for _ in range(700):
+    r_sun.append(sun.position)
+    r_earth.append(earth.position)
+    r_mars.append(mars.position)
+
+    universe.update()
+
+plt.figure()
+plt.plot([r.x for r in r_sun], [r.y for r in r_sun], 'o', label='Sun trajectory')
+plt.plot([r.x for r in r_earth], [r.y for r in r_earth], label='Earth trajectory')
+plt.plot([r.x for r in r_mars], [r.y for r in r_mars], label='Mars trajectory')
+plt.legend()
+plt.show()
 
