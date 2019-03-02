c100f56be9c90c678054e50b854babffcf49e940
Daan de Ruiter <daanjderuiter@hotmail.nl>
2019-03-02 15:22:29 +0100

    Modified notebook, added vector class, added MATLAB-code example

---
+++ student.m
@@ -0,0 +1,22 @@
+classdef student
+  properties (Constant)
+    associations = struct(...
+    'Applied Physics', 'Arago', ...
+    'Computer Science', 'Inter-actief', ...
+    'Chemical Engineering', 'alembic');
+  end
+  
+  methods
+    % The constructor
+    function obj = student(name, student_id, programme)
+      obj.name = name;
+      obj.student_id = student_id;
+      obj.programme = programme;
+    end
+    
+    function a = getAssociation()
+        a = obj.associations(obj.programme);
+    end
+  end
+  
+end
